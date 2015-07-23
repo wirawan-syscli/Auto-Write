04a29c15047ea70d78b3c970d6f57cf787274482
wirawan-syscli <wirawan@syscli.co.jp>
2015-07-23 17:16:32 +0900

    15/07/23 - 1.0.18

--- Auto Write/Auto Write-Bridging-Header.h
+++ Auto Write/Auto Write-Bridging-Header.h
@@ -2,7 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <TesseractOCR/TesseractOCR.h>
+// #import <TesseractOCR/TesseractOCR.h>
 
 #import <MagicalRecord/MagicalRecord.h>
 
